e81a9beee947c4ca911109a8e8a9398bdbd06e68
rey2424 <rey.24@hotmail.it>
2020-04-27 10:00:16 +0200

    adding

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component{
   onButtonSubmit = () => {
     
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://ancient-beyond-23659.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -102,7 +102,7 @@ class App extends Component{
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://ancient-beyond-23659.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
